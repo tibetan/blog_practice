6af1d45c9d63ff88d68fb21844d9c00c641f6236
Yurii M <tibet.mart@gmail.com>
2015-11-29 14:06:05 +0200

    Develop. Create seed for items table..

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-         $this->call('');
+         $this->call('ItemsTableSeeder');
 
-        Model::reguard();
+//        Model::reguard();
     }
 }
---
+++ database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Item;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->delete();
+        
+        for ($i = 1; $i <= 5; $i++) {
+            Item::create([
+                'header'   => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+                'article'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            ]);
+        }
+
+        $this->command->info('Data to items table was added');
+    }
+}
