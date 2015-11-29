de5cad10af3f201857f73147f0dfdd071dd70790
Yurii M <tibet.mart@gmail.com>
2015-11-29 09:52:35 +0200

    Develop. Create and comment develop bar.

--- config/app.php
+++ config/app.php
@@ -144,6 +144,11 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * Debug bar
+         */
+//        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -193,6 +198,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+//        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
 
     ],
 
