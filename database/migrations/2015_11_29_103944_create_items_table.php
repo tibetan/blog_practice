ac858abde30e378e71c7014b356112860f8cd59a
Yurii M <tibet.mart@gmail.com>
2015-11-30 14:34:39 +0200

    Develop. Change column name in table.

--- database/migrations/2015_11_29_103944_create_items_table.php
+++ database/migrations/2015_11_29_103944_create_items_table.php
@@ -15,7 +15,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
             $table->string('header', 255);
-            $table->text('article');
+            $table->text('body');
             $table->timestamps();
         });
     }
