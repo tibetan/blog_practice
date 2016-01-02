74815051856284ae9b03f75d40cd4ae417d6d018
Yurii M <tibet.mart@gmail.com>
2016-01-02 19:20:07 +0200

    feature. 4.  Add migtation and model for table "rubrics".

---
+++ app/Rubric.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rubric extends Model
+{
+    /**
+     * The name of the "name" column.
+     *
+     * @var string
+     */
+    const NAME = 'name';
+}
