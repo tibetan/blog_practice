c57d2829970dfb34dfb9ea46d888ef18acad1130
Yurii M <tibet.mart@gmail.com>
2016-01-03 11:04:36 +0200

    Develop. Fix Item page if no data item.

--- resources/views/item/item/show.blade.php
+++ resources/views/item/item/show.blade.php
@@ -1,21 +1,23 @@
 @extends('app')
 
 @section('item-item-show-content')
-    <div class="page-header">
-        <a href="#">
-            <img src="{{ asset('img/thailand.jpg') }}" width="50" height="50" alt="user logo" class="img-circle">
-        </a>
-        <h2>{{ $item->getAttributeValue($item::HEADER) }}</h2>
-    </div>
-    <div>
-        <p>{{ $item->getAttributeValue($item::UPDATED_AT)->format(Lang::get('messages.date')) }}</p>
-    </div>
-    <hr>
-    <p>
-        {{ $item->getAttributeValue($item::BODY) }}
-    </p>
-    <hr>
-    <div>
-        <a href="#">Comments <span class="badge right">42</span></a>
-    </div>
+    @if ($item)
+        <div class="page-header">
+            <a href="#">
+                <img src="{{ asset('img/thailand.jpg') }}" width="50" height="50" alt="user logo" class="img-circle">
+            </a>
+            <h2>{{ $item->getAttributeValue($item::HEADER) }}</h2>
+        </div>
+        <div>
+            <p>{{ $item->getAttributeValue($item::UPDATED_AT)->format(Lang::get('messages.date')) }}</p>
+        </div>
+        <hr>
+        <p>
+            {{ $item->getAttributeValue($item::BODY) }}
+        </p>
+        <hr>
+        <div>
+            <a href="#">Comments <span class="badge right">42</span></a>
+        </div>
+    @endif
 @stop
